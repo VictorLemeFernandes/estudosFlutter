9330ab38dc766b1cfda3b4c7c25c48d37d490596
VictorLemeFernandes <victorlemefernandes@gmail.com>
2022-05-12 14:32:24 -0300

    Contato com API e criacao do Loader

--- cursoDeividWillyan/personalizedsWidgets/exemplo005/lib/first_page.dart
+++ cursoDeividWillyan/personalizedsWidgets/exemplo005/lib/first_page.dart
@@ -1,7 +1,9 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:exemplo005/widgets/custom_button_widget.dart';
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
@@ -11,12 +13,23 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
-  ValueNotifier<int> valorAleatorio = ValueNotifier<int>(0);
+  ValueNotifier<List<Post>> posts = ValueNotifier<List<Post>>([]);
+  ValueNotifier<bool> inLoader = ValueNotifier<bool>(false);
 
-  random() async {
-    for (var i = 0; i < 10; i++) {
-      await Future.delayed(const Duration(seconds: 1));
-      valorAleatorio.value= Random().nextInt(1000);
+  callAPI() async {
+    var client = http.Client();
+    try {
+      inLoader.value = true;
+      var response = await client.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      );
+
+      var decodedResponse = jsonDecode(response.body) as List;
+      posts.value = decodedResponse.map((e) => Post.fromJson(e)).toList();
+      await Future.delayed(const Duration(seconds: 2));
+    } finally {
+      client.close();
+      inLoader.value = false;
     }
   }
 
@@ -24,30 +37,59 @@ class _FirstPageState extends State<FirstPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ValueListenableBuilder( //Substituto do setState (ñ reinicializa tudo toda hora)
-              valueListenable: valorAleatorio,
-              builder: (_, valor, __) => Text(
-                'Valor é: $valor',
-                style: const TextStyle(fontSize: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                //Substituto do setState (ñ reinicializa tudo toda hora)
+                animation: Listenable.merge([posts, inLoader]),
+                builder: (_, __) => inLoader.value
+                    ? const CircularProgressIndicator()
+                    : ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: posts.value.length,
+                        itemBuilder: (_, idx) => ListTile(
+                          title: Text(posts.value[idx].title),
+                        ),
+                      ),
               ),
-            ),
 
-            const SizedBox(height: 10,), //Cria o espaço de altura
+              const SizedBox(
+                height: 10,
+              ), //Cria o espaço de altura
 
-            CustomButtonWidget(
-              disableButton: false,
-              onPressed: () {
-                random();
-              },
-              title: 'Custom BTN',
-              titleSize: 18,
-            ),
-          ],
+              CustomButtonWidget(
+                disableButton: false,
+                onPressed: () {
+                  callAPI();
+                },
+                title: 'Custom BTN',
+                titleSize: 18,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(this.userId, this.id, this.title, this.body);
+
+  factory Post.fromJson(Map json) {
+    return Post(json['userId'], json['id'], json['title'], json['body']);
+  }
+
+  @override
+  String toString() {
+    return 'id: $id';
   }
 }
